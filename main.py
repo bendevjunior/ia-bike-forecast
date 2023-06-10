6bb0895699228953dec5bcc96a1c79a467b95e4a
Bendev Junior <bendevoficial@gmail.com>
2023-06-10 19:45:00 +0000

    iniciando treinamento do modelo

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ bike = pd.read_csv('bike-sharing-daily.csv')
 #sns.heatmap(bike.isnull())
 #print(bike.head())
 
+# drop the columns if null
+
 bike = bike.drop(['instant', 'casual', 'registered'], axis=1)
 bike.dteday = pd.to_datetime(bike.dteday, format='%m/%d/%Y')
 bike.index = pd.DatetimeIndex(bike.dteday)
@@ -34,24 +36,32 @@ onehotencoder = OneHotEncoder()
 x_cat = onehotencoder.fit_transform(x_cat).toarray()
 x_cat = pd.DataFrame(x_cat)
 
-x_numerical.reset_index()
+x_numerical = x_numerical.reset_index()
 #print(x_numerical.head())
 
+
 x_all = pd.concat([x_cat, x_numerical], axis=1)
+
+# verifica se existe algum com valor NAN
 #print(x_all.head())
-#x_all = x_all.drop(labels=['dteday'], axis=1)
+x_all = x_all.drop(labels=['dteday'], axis=1)
 
 x = x_all.iloc[:, :-1].values
-y = x_all.iloc[:, -1].values
+y = x_all.iloc[:, -1:].values
 #print(y)
 
 scaler = MinMaxScaler()
-y = scaler.fit_transform(y.reshape(-1, 1))
+y = scaler.fit_transform(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-print(x_train.shape)    # (584, 34)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(units=100, activation='relu', input_shape=(x_train.shape[1],)))
 model.add(tf.keras.layers.Dense(units=100, activation='relu'))
 model.add(tf.keras.layers.Dense(units=1, activation='linear'))
+
+print(model.summary())
+
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='mean_squared_error')
+
+epochs_hist = model.fit(x_train, y_train, epochs=25, batch_size=50, validation_split=0.2)
